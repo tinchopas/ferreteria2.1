d31c43c7c94a26a32304ae5eaad14686efc2966f
Damian <enviaaca@gmail.com>
2012-08-26 20:51:23 -0300

    Revert "Agregado bundle de Base de Datos"
    This reverts commit 42205b3e6962fe212b51650f51db0166f0d62574.

--- app/autoload.php
+++ app/autoload.php
@@ -9,7 +9,6 @@ if (!function_exists('intl_get_error_code')) {
     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
 
     $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
-    $loader->add('', __DIR__.'/../vendor/bundles/ENC/Bundle/BackupRestoreBundle');
 }
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
