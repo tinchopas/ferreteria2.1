bea05a59677c735f6b2bec7aba9ba680a09057d5
root <root@squeeze.(none)>
2012-09-24 03:42:35 -0300

    Se corrigen errores en composer.json y se agrega la instalacion de BackupRestoreBundle por medio de composer

--- app/autoload.php
+++ app/autoload.php
@@ -4,13 +4,6 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 
-
-//// Se agrega hasta que se solucione el issue:
-//// https://github.com/comfortablynumb/BackupRestoreBundle/issues/1
-    $loader->add('ENC',__DIR__.'/../vendor/bundles');
-    $loader->register();
-////
-
 // intl
 if (!function_exists('intl_get_error_code')) {
     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
